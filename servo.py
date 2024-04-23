60d282ad6d06e0f980679eb4202d098f9d469530
Alaukkan <165171862+Alaukkan@users.noreply.github.com>
2024-04-23 11:45:04 +0300

    Update servo.py

--- servo.py
+++ servo.py
@@ -1,7 +1,6 @@
 # Basic servo library found here:
 # https://forums.raspberrypi.com/viewtopic.php?t=303900
 
-import pca9685
 import math
 from machine import Pin, PWM
 import utime
@@ -51,36 +50,3 @@ class Servo:
         self.__angle_conversion_factor = (self.__max_u16_duty - self.__min_u16_duty) / (self.max_angle - self.min_angle)
         self.__motor = PWM(Pin(pin))
         self.__motor.freq(self.__servo_pwm_freq)
-
-
-class Servos:
-    def __init__(self, i2c, address=0x40, freq=50, min_us=600, max_us=2400,
-                 degrees=180):
-        self.period = 1000000 / freq
-        self.min_duty = self._us2duty(min_us)
-        self.max_duty = self._us2duty(max_us)
-        self.degrees = degrees
-        self.freq = freq
-        self.pca9685 = pca9685.PCA9685(i2c, address)
-        self.pca9685.freq(freq)
-
-    def _us2duty(self, value):
-        return int(4095 * value / self.period)
-
-    def position(self, index, degrees=None, radians=None, us=None, duty=None):
-        span = self.max_duty - self.min_duty
-        if degrees is not None:
-            duty = self.min_duty + span * degrees / self.degrees
-        elif radians is not None:
-            duty = self.min_duty + span * radians / math.radians(self.degrees)
-        elif us is not None:
-            duty = self._us2duty(us)
-        elif duty is not None:
-            pass
-        else:
-            return self.pca9685.duty(index)
-        duty = min(self.max_duty, max(self.min_duty, int(duty)))
-        self.pca9685.duty(index, duty)
-
-    def release(self, index):
-        self.pca9685.duty(index, 0)
